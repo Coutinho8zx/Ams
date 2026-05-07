a50a56cf9bf5e4d506d59d54da070cfd7ccbe682
Coutinho8zx <coutinho20110@gmail.com>
2026-05-07 13:37:37 -0300

    Add files via upload

--- curriculo/JS/script.js
+++ curriculo/JS/script.js
@@ -6,13 +6,13 @@ const botao = document.getElementById("Tema");
          }
 
          //alternar tema
-         botao.addEventListener("click", () => {
+         botao.addEventListener("click",() => {
             document.body.classList.toggle("dark");
 
             if (document.body.classList.contains("dark")) {
-                localStorage.setItem("tema", "dark");
+                localStorage.setItem("Tema", "dark");
             } else {
-                localStorage.setItem("tema", "light");
+                localStorage.setItem("Tema", "light");
             }
          });
          
